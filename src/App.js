0ea1169fbe9656cbf89370268fe6066d589bf096
Sam Mariassouce <sammariassouce@gmail.com>
2023-07-24 09:32:49 -1000

    form

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { findRenderedDOMComponentWithClass } from 'react-dom/test-utils';
+
 const initialFriends = [
   {
     id: 118836,
@@ -21,8 +23,62 @@ const initialFriends = [
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="app">
+      <div className="side-bar">
+        <FriendsList />
+        <FormAddFriend />
+        <Button>Add friend</Button>
+      </div>
     </div>
+  );
+}
+
+function FriendsList() {
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}$
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {Math.abs(friend.balance)}$
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+      <Button>Select</Button>
+    </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>😍 Friend name</label>
+      <input type="text" />
+
+      <label>🌄 Image URL</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
   );
 }
