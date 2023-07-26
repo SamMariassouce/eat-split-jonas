a8951ca0a36efc04c94c78a8efdad145b706403b
Sam Mariassouce <sammariassouce@gmail.com>
2023-07-26 09:24:34 -1000

    fddg

--- src/App.js
+++ src/App.js
@@ -44,7 +44,8 @@ export default function App() {
   }
 
   function handleSelection(friend) {
-    setSelectedFriend(friend);
+    setSelectedFriend((cur) => (cur?.id === friend.id ? null : friend));
+    setShowAddFriend(false);
   }
 
   return (
@@ -82,7 +83,7 @@ function FriendsList({ friends, onSelection, selectedFriend }) {
 }
 
 function Friend({ friend, onSelection, selectedFriend }) {
-  const isSelected = selectedFriend.id === friend.id;
+  const isSelected = selectedFriend?.id === friend.id;
   return (
     <li className={isSelected ? 'selected' : ''}>
       <img src={friend.image} alt={friend.name} />
@@ -99,7 +100,9 @@ function Friend({ friend, onSelection, selectedFriend }) {
         </p>
       )}
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
-      <Button onClick={() => onSelection(friend)}>Select</Button>
+      <Button onClick={() => onSelection(friend)}>
+        {isSelected ? 'close' : 'select'}
+      </Button>
     </li>
   );
 }
